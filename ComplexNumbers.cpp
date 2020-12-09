cf192a4984be6055416e54787d03d0bc3e924b0b
Husam Nujaim <60360833+HussamNujaim@users.noreply.github.com>
2020-12-09 18:39:27 +0100

    Ex. 3
    Perform addition operation on complex data using class and object. The program should ask for real and imaginary part of two complex numbers, and display the real
    and imaginary parts of their sum.

---
+++ ComplexNumbers.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ComplexNumber
+{
+private:
+    int r; //real
+    int i; //imaginary
+public:
+    ComplexNumber(int r,int i) //Constructor to initialize the numbers
+    {
+        this->r=r;
+        this->i=i;
+    }
+    int GetRealNumber()
+    {
+        return r;
+    }
+    int GetImaginaryNumber()
+    {
+        return i;
+    }
+};
+
+class ComplexOperations
+{
+public:
+    string Sum(ComplexNumber a, ComplexNumber b)
+    {
+        int r = a.GetRealNumber() + b.GetRealNumber();
+        int i = a.GetImaginaryNumber() + b.GetImaginaryNumber();
+        return std::to_string(r) + "+"+std::to_string(i)+"i"; // return the sum of the 2 complex numbers
+    }
+};
+
+int main()
+{
+    int r1,r2,i1,i2;
+    std::cout<<"Enter The 1st Real Number:"<<std::endl;
+    std::cin>>r1;
+    std::cout<<"Enter The 1st Imaginary Number:"<<std::endl;
+    std::cin>>i1;
+    std::cout<<"Enter The 2nd Real Number:"<<std::endl;
+    std::cin>>r2;
+    std::cout<<"Enter The 2nd Imaginary Number:"<<std::endl;
+    std::cin>>i2;
+    ComplexNumber c1(r1,i1);
+    ComplexNumber c2(r2,i2);
+    ComplexOperations complex;
+    string sum = complex.Sum(c1,c2);
+    cout << "The Sum = " <<sum<< endl;
+    return  0;
+}
